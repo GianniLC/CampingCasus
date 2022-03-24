d241a6f6d032ea6f959d690f135bec6b66d7416a
GianniLC <70944324+GianniLC@users.noreply.github.com>
2022-03-24 11:43:23 +0100

    new layout changed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,18 @@
 
 @include('components.layout.__header')
 
-<body>
+<x-layout.__layout>
+    <x-slot name="admin"></x-slot>
+
+    <body>
     <p>Hallo</p>
 
     <a href="/users">Show users</a>
     <a href="/register">register</a>
 
-</body>
+    </body>
+
+    <x-slot name="extra"></x-slot>
+</x-layout.__layout>
+
+
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,4 +1,27 @@
-@include('componegit nts.layout.__header')
+@include('components.layout.__header')
+
+<x-layout.__layout>
+    <x-slot name="admin"></x-slot>
+    <table class="border-2 border-collapse border-gray-500 shadow-lg w-full">
+        <tr class="text-left">
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Email</th>
+            <th>phonenumber</th>
+            <th>Checked in</th>
+        </tr>
+        @foreach($user as $users)
+
+            <tr>
+                <td class="w-24">{{ ucwords($users->firstname) }}</td>
+                <td class="w-24">{{ ucwords($users->lastname) }}</td>
+                <td class="w-64">{{ $users->email }}</td>
+                <td class="w-24">{{ $users->phonenumber }}</td>
+            </tr>
+        @endforeach
+    </table>
+    <x-slot name="extra"></x-slot>
+</x-layout.__layout>
 
 <div>
     <a href="/"> home </a>
@@ -15,21 +38,4 @@
 </style>
 
 
-<table class="border-2 border-collapse border-gray-500 shadow-lg w-full">
-    <tr class="text-left">
-        <th>First name</th>
-        <th>Last name</th>
-        <th>Email</th>
-        <th>phonenumber</th>
-        <th>Checked in</th>
-    </tr>
-    @foreach($user as $users)
 
-    <tr>
-        <td class="w-24">{{ ucwords($users->firstname) }}</td>
-        <td class="w-24">{{ ucwords($users->lastname) }}</td>
-        <td class="w-64">{{ $users->email }}</td>
-        <td class="w-24">{{ $users->phonenumber }}</td>
-    </tr>
-    @endforeach
-</table>
